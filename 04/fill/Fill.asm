4f971871be1fd563d479be411dc806e05b6895b8
hooo0101 <a0966482249@gmail.com>
2024-12-29 14:53:20 +0000

    04

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,38 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+@SCREEN
+M=D        
+(LOOP)
+    @KBD
+    D=M        
+    @CHECK_KEY
+    D;JNE     
+    @WHITE
+    0;JMP       
+
+(CHECK_KEY)
+    @BLACK
+    0;JMP       
+
+(WHITE)
+    @SCREEN
+    M=D         
+    D=0xFFFF    
+    @SCREEN_LOOP
+    M=D
+    @SCREEN
+    D=M+1
+    @SCREEN_LOOP
+    @LOOP
+    0;JMP
+
+(BLACK)
+    @SCREEN
+    M=D         
+    D=0x0000    
+    @SCREEN_LOOP2
+    M=D
+    @SCREEN
+    D=M+1 
+    @SCREEN_LOOP2
